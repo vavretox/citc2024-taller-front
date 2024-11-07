38cb8e680c085fc31d94f5cd2e07a81496872e58
Samuel Loza <samuel.loza26@gmail.com>
2024-11-07 03:33:29 -0400

    actualizar lint

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,10 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isCustom?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -22,4 +23,5 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = "Input"
 
-export { Input }
+export { Input };
+
